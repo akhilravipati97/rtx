e5b0892ee0897ca9aabee7d73bdb310ea855917c
Akhil Ravipati <akhil.ravipati.o@gmail.com>
2022-03-24 18:42:18 -0400

    Init commit

---
+++ src/test/java/com/reddit/top/RtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reddit.top;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
